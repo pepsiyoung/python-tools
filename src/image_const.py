5a5cc403cc164567bdcc9b5c09d2c14f24f4da93
pepsiyoung <pepsiyoung@icloud.com>
2022-08-24 16:43:44 +0800

    image_const.py finish

--- src/image_const.py
+++ src/image_const.py
@@ -4,6 +4,7 @@ from PIL import Image
 from PIL import ImageChops
 from pathlib import Path
 from tqdm import tqdm
+import imagehash
 
 
 def parse_opt(known=False):
@@ -13,25 +14,40 @@ def parse_opt(known=False):
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
 
-# 图片去重
-if __name__ == '__main__':
-    opt = parse_opt(True)
-
-    repeat_list = []
-    im_paths = Path(opt.source).glob('**/*.jpg')
-    im_list = [(path, Image.open(path)) for path in im_paths]
-
-    for index, im in enumerate(tqdm(im_list[:-1])):
+def deep_compare(im_list):
+    for index, im in enumerate(im_list[:-1]):
         for next_im in im_list[index + 1:]:
             try:
                 diff = ImageChops.difference(im[1], next_im[1])
                 if diff.getbbox() is None:
                     # 相同
                     repeat_list.append(im[0])
+                    # print('相同', im[0], next_im[0])
                     break
             except ValueError as e:
                 print(e, im[0], next_im[0])
 
+
+# 图片去重
+if __name__ == '__main__':
+    opt = parse_opt(True)
+    repeat_list = []
+    my_dict = {}
+    print('正在加载图像，请稍等...')
+    im_paths = list(Path(opt.source).glob('**/*.jpg'))
+    im_list = [(path, Image.open(path).convert('L')) for path in im_paths]
+
+    print('正在去重...')
+    for item in im_list:
+        path, im = item
+        key = imagehash.dhash(im)
+        my_dict.setdefault(str(key), []).append(item)
+
+    new_dict = {k: v for k, v in my_dict.items() if len(v) > 1}
+    for k, v in tqdm(new_dict.items()):
+        deep_compare(v)
+
     for path in repeat_list:
         shutil.move(path, opt.target)
-    print('相同图片数：{}'.format(len(repeat_list)))
+
+    print('总数:{} 重复:{}'.format(len(im_paths), len(repeat_list)))
