bfaec4a5664c32565e70e769edaf4608d142c359
pepsiyoung <pepsiyoung@icloud.com>
2022-09-15 14:42:16 +0800

    add lrucache

--- src/test1.py
+++ src/test1.py
@@ -29,21 +29,4 @@ def check_requirements(name):
 
 
 if __name__ == '__main__':
-    # check_requirements('vvv')
-    values_cnt = {}
-
-    l_count = 0
-    other_count = 0
-    im_path = r'D:\datasets\CSI_0901\images'
-    im_paths = Path(im_path).glob('**/*.jpg')
-    for im_path in tqdm(list(im_paths)):
-        im = Image.open(im_path)
-        # im.convert("RGB").save(Path("D:\\datasets\\CSI_0901\\images_rgb").joinpath(im_path.name))
-
-        if im.mode == 'L':
-            l_count += 1
-        else:
-            other_count += 1
-
-    print('l_count:', l_count)
-    print('other_count:', other_count)
+    check_requirements('zcy')
--- src/test2.py
+++ src/test2.py
@@ -1,36 +1,15 @@
-import numpy as np
-from PIL import Image
-from pathlib import Path
+def find_pair(arr: list, d):
+    cache = {}
+    for index, item in enumerate(arr):
+        cache[item] = index
 
-# im = Image.open(r'E:\temp\1.jpg').convert('RGB')
-# im_data = np.array(im)
-# print(im_data.shape)
-# print(im.mode)
-# im_data_new = np.concatenate((im_data[:1400, ::-1, :], im_data[1400:, :, :]), axis=0)
-# print(im_data_new.shape)
-# #
-# im_target = Image.fromarray(im_data_new)
-# im_target.show()
-
-# n1 = np.array([[1, 2, 3], [4, 5, 6]])
-# print(n1.shape)
-# n2 = np.array([[7, 8, 9]])
-# print(n2.shape)
-# n3 = np.concatenate((n1, n2), axis=0)
-# print(n3)
+    print(cache)
+    for i in range(len(arr)):
+        key = abs(d - arr[i])
+        if key in cache.keys() and cache.get(key) != i:
+            return i, cache.get(key)
+    return None
 
 
-# def a():
-#     param = 'b'  # 这里就会出现这样的提示，因为在main定义的param对象被重新指定了新的值
-#     print(param)
-#
-#
-# if __name__ == '__main__':
-#     param = 'a'
-#     a()
-
-
-a = (1, 2, 3, 4)
-
-for item in a:
-    print(type(item))
+res = find_pair([50, 100, 150], 50)
+print(res)
