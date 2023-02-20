bb2f1edb6bd3b4465e7bad9d2f12f4d117d2413f
pepsiyoung <pepsiyoung@icloud.com>
2023-02-20 16:07:53 +0800

    file_move_labels.py upgrade

--- src/file/file_move_labels.py
+++ src/file/file_move_labels.py
@@ -9,6 +9,8 @@ def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=str, default=r'./source', help='labels源文件夹')
     parser.add_argument('--target', type=str, default=r'./target', help='labels目标文件夹')
+    parser.add_argument('--filter-cls', type=str, default=['4', '6'])
+    parser.add_argument('--sample-move', action='store_true', help='设置使用 move 或 copy')
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
 
@@ -17,24 +19,13 @@ if __name__ == '__main__':
     txt_paths = list(Path(opt.source).glob('*.txt'))
     wait_process = list()
     for txt_path in tqdm(list(txt_paths)):
-        move_flag = False
         with open(txt_path, 'r') as f:
             lines = f.readlines()
             x = [line[0] for line in lines]
-            if '4' in x or '6' in x:
+            if len(set(opt.filter_cls).intersection(set(x))) > 0:
                 wait_process.append(txt_path)
-                move_flag = True
 
-                # shutil.copy(Path(txt_path), Path(opt.target))
-        if move_flag:
-            shutil.move(Path(txt_path), Path(opt.target))
-
+    # 选择是 move 还是 copy
+    sample_process = shutil.move if opt.sample_move else shutil.copy
     for txt_path in wait_process:
-        shutil.copy(Path(txt_path), Path(opt.target))
-
-        # with open(txt_path, 'r') as f:
-        #     lines = (i for i in f.readlines() if int(i[0]) < 4)
-        #     file_path = Path(opt.target).joinpath(txt_path.name)
-        #     file_new = open(file_path, 'w')
-        #     file_new.writelines(lines)
-        #     file_new.close()
+        sample_process(Path(txt_path), Path(opt.target))
