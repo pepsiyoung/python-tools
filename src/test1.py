c9f96e84ee8e1bbf2ba4225224f3ab3deeffc48b
pepsiyoung <pepsiyoung@icloud.com>
2023-04-10 09:20:29 +0800

    RTXA6000 PC commit 04-10

--- src/test1.py
+++ src/test1.py
@@ -1,48 +1,49 @@
-from PIL import Image
-from pathlib import Path
-import yaml
-from tqdm import tqdm
+import torch
+from torch import nn
+from torch.nn import functional as F
 
 
-def test(func):
-    def handler(*args, ):
-        print('test')
-        func(*args)
+class Residual(nn.Module):
+    def __init__(self, input_channels, num_channels,
+                 use_1x1conv=False, strides=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.bn2 = nn.BatchNorm2d(num_channels)
 
-    return handler
-
-
-def say_hello(country):
-    def wrapper(func):
-        def decorate(*args, **kw):
-            print('country:', country)
-            func(*args, **kw)
-
-        return decorate
-
-    return wrapper
-
-
-# def try_except(func):
-#     # try-except function. Usage: @try_except decorator
-#     def handler(*args, **kwargs):
-#         try:
-#             func(*args, **kwargs)
-#         except Exception as e:
-#             print(e)
-#
-#     return handler
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return F.relu(Y)
 
 
-# @test
-country = 'usa'
+class SPPF(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.maxpool = nn.MaxPool2d(5, 1, padding=2)
 
-
-@say_hello(country)
-def check_requirements(name):
-    print('check_requirements:' + name)
+    def forward(self, x):
+        o1 = self.maxpool(x)
+        print('o1_shape', o1.shape)
+        o2 = self.maxpool(o1)
+        print('o2_shape', o2.shape)
+        o3 = self.maxpool(o2)
+        return torch.cat([x, o1, o2, o3], dim=1)
 
 
 if __name__ == '__main__':
-    check_requirements('zcy')
-    check_requirements('ccc')
+    input_tensor = torch.rand(8, 32, 16, 16)
+    # sppf = SPPF()
+    # output = sppf(input_tensor)
+    # print(output.shape)
+
+    net = nn.MaxPool2d(5, 1, padding=2)
+    print(net(input_tensor).shape)
