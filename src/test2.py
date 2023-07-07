440a53dbaa7814f22222b8a29b7d631638bf3eef
pepsiyoung <pepsiyoung@icloud.com>
2023-07-07 09:09:00 +0800

    commit 0707

--- src/test2.py
+++ src/test2.py
@@ -23,11 +23,24 @@ def lru_cache():
 def create_delay_image():
     im_path = r"C:\Users\admin\Desktop\待检测图\43-000235-A.jpg"
 
-    write_file = open(r"D:\Test\source1\name.jpg", "wb")
+    write_file = open(r"D:\Test\source1\name-A.jpg", "wb")
     with open(im_path, "rb") as f:
         for index, line in enumerate(f.readlines()):
             write_file.write(line)
             time.sleep(0.02)
+            print(index)
+
+    write_file.close()
+    print('end')
+
+
+def create_delay_image(im_path, interval_second):
+    file_name = Path(im_path).stem
+    write_file = open(rf"D:\Test\source1\{file_name}.jpg", "wb")
+    with open(im_path, "rb") as f:
+        for index, line in enumerate(f.readlines()):
+            write_file.write(line)
+            time.sleep(interval_second)
             print(index)
 
     write_file.close()
@@ -42,11 +55,12 @@ def use_np_c():
 
 
 if __name__ == '__main__':
-    source = r'D:\Datasets\DataSample\层后0411\images'
-    target = r'D:\Datasets\DataSample\层后0411\images1'
-    txt_paths = list(Path(source).glob('*.jpg'))
-    for txt_path in tqdm(list(txt_paths)):
-        im = Image.open(txt_path)
-        w, h = im.size
-        if w == 3800 and h == 2200:
-            shutil.copy(Path(txt_path), Path(target))
+    # source = r'D:\Datasets\DataSample\层后0411\images'
+    # target = r'D:\Datasets\DataSample\层后0411\images1'
+    # txt_paths = list(Path(source).glob('*.jpg'))
+    # for txt_path in tqdm(list(txt_paths)):
+    #     im = Image.open(txt_path)
+    #     w, h = im.size
+    #     if w == 3800 and h == 2200:
+    #         shutil.copy(Path(txt_path), Path(target))
+    create_delay_image(r"D:\Test\14-000014-B.jpg", 0.1)
